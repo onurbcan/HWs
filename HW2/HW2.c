14ba95b0df5906d0adbee1fc1a293fb602adc257
Onur <onuurcan@gmail.com>
2020-02-28 16:43:20 +0100

    part1 done

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -14,25 +14,7 @@
 #include "Part1.h"
 
 int main(void) {
-	int side1, side2, side3;
-	side1 = 0;
-	side2 = 0;
-	side3 = 0;
-
-	printf("The length of first side: \n");
-	scanf("%d\n",&side1);
-	printf("The length of second side: \n");
-	scanf("%d\n",&side2);
-	printf("The length of third side: \n");
-	scanf("%d\n",&side3);
-
-	if(draw_triangle(side1, side2, side3) == 1) printf("According to the triangle inequality theorem, this triangle can be drawn.\n");
-	else printf("According to the triangle inequality theorem, this triangle cannot be drawn.\n");
-
-	type_triangle(side1, side2, side3);
-	printf("The perimeter of the triangle: %d\n", perimeter_triangle(side1, side2, side3));
-	printf("The area of the triangle: %f", area_triangle(side1, side2, side3));
-
+	triangle_test();
 
 	return EXIT_SUCCESS;
 }
--- HW2/inc/Part1.h
+++ HW2/inc/Part1.h
@@ -8,6 +8,7 @@
 #ifndef PART1_H_
 #define PART1_H_
 
+void triangle_test();
 int draw_triangle();
 void type_triangle();
 int perimeter_triangle();
