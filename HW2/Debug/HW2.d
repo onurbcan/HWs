9ba3ded8b52bc8881d929a04768ec8c49342a2e6
Onur <onur.can@ozu.edu.tr>
2020-03-01 20:48:01 +0100

    unnecessary files have been removed

--- HW2/Debug/HW2.d
+++
@@ -1,3 +0,0 @@
-HW2.o: ../HW2.c /home/onur/Documents/GIT_Repos/HWs/HW2/inc/Part3.h
-
-/home/onur/Documents/GIT_Repos/HWs/HW2/inc/Part3.h:
--- HW2/Debug/src/Part3.d
+++
@@ -1,4 +0,0 @@
-src/Part3.o: ../src/Part3.c \
- /home/onur/Documents/GIT_Repos/HWs/HW2/inc/Part3.h
-
-/home/onur/Documents/GIT_Repos/HWs/HW2/inc/Part3.h:
