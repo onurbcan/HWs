5bf6d59f4fb5c607dd2bd8beb3c9b36ec17c9741
Onur <onur.can@ozu.edu.tr>
2020-03-01 20:51:41 +0100

    additional unnecessary files have been removed

--- HW1Prev/Debug/HW1Prev.d
+++
@@ -1,4 +0,0 @@
-HW1Prev.o: ../HW1Prev.c \
- /home/onur/Documents/GIT_Repos/HWs/HW1Prev/inc/Assignment1.h
-
-/home/onur/Documents/GIT_Repos/HWs/HW1Prev/inc/Assignment1.h:
--- HW1Prev/Debug/src/Assignment1.d
+++
@@ -1,4 +0,0 @@
-src/Assignment1.o: ../src/Assignment1.c \
- /home/onur/Documents/GIT_Repos/HWs/HW1Prev/inc/Assignment1.h
-
-/home/onur/Documents/GIT_Repos/HWs/HW1Prev/inc/Assignment1.h:
