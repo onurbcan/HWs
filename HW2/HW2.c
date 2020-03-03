4c1607a85f6543516184482cdd2f2a9aa27dc91a
Onur <onur.can@ozu.edu.tr>
2020-03-03 22:35:50 +0100

    main file updated

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -11,13 +11,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//#include "Part1.h"
-//#include "Part2.h"
+#include "Part1.h"
 #include "Part3.h"
 
 int main(void) {
-	//triangle_test();
-	//number_test();
+	triangle_test();
 	currency_converter();
 
 	return EXIT_SUCCESS;
