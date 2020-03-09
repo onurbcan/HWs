c163949e1dc1f50148551ae96c4d512dc5d63259
Onur <onur.can@ozu.edu.tr>
2020-03-09 23:30:14 +0100

    part2 completed

--- HW3/Debug/HW3.d
+++ HW3/Debug/HW3.d
@@ -1,3 +1,3 @@
-HW3.o: ../HW3.c /home/onur/Documents/GIT_Repos/HWs/HW3/inc/Part1.h
+HW3.o: ../HW3.c /home/onur/Documents/GIT_Repos/HWs/HW3/inc/Part2.h
 
-/home/onur/Documents/GIT_Repos/HWs/HW3/inc/Part1.h:
+/home/onur/Documents/GIT_Repos/HWs/HW3/inc/Part2.h:
