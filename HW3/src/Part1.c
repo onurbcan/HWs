1830ee3c5e6288c40f125a586cfcf0b55e135076
Onur <onur.can@ozu.edu.tr>
2020-03-09 21:47:37 +0100

    description edited

--- HW3/src/Part1.c
+++ HW3/src/Part1.c
@@ -1,5 +1,15 @@
 /*
  * Part1.c
+ * Description: In the main function, the program respectively request an Integer
+ * (N), an operation flag, and finally a flag to decide if it is going to work on
+ * odd / even numbers. The program will support only 2 operations, addition or
+ * multiplication. According to the selection of the flags, the program will
+ * calculate the sum/multiplication of the odd/even numbers between the range of
+ * [1,N]. The addition and the multiplication operations should be done by using
+ * two different functions. The operation selection should be determined by using
+ * operation flag with switch-case, after that, the integer and the other flag
+ * should be used to call the related function. If user enters invalid value for
+ * the flags, the program should print an error message.
  *
  *  Created on: Mar 9, 2020
  *      Author: onur
