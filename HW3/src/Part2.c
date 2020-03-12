76d83a2dda0536ce970eb416633c79a98b5ff66d
Onur <onur.can@ozu.edu.tr>
2020-03-12 20:59:56 +0100

    some fixes

--- HW3/src/Part2.c
+++ HW3/src/Part2.c
@@ -38,10 +38,9 @@ void check_prime(){
 }
 
 int isprime(int a){
-	int i, flag;
+	int i, flag = 0; //this is set 0 to make 2 (exceptional) as a prime number;
 	double root_a;
 
-	flag = 0; //this is set 0 to make 2 (exceptional) as a prime number
 	root_a = sqrt(a); //control range for the prime check
 
 	for(i = 2 ; i <= root_a ; ++i){
@@ -51,7 +50,8 @@ int isprime(int a){
 				/* this if case is to keep -not a prime number- if it is already
 				 * not a prime number
 				 */
-			else flag = 0; //0 flag for prime numbers
+			else
+				flag = 0; //0 flag for prime numbers
 		}
 		else
 			flag = i; //different than 0 flag, to return divider
