0350fa1eddcbb1826c0a98537eac0fbc14524d10
Onur <onur.can@ozu.edu.tr>
2020-03-12 21:55:03 +0100

    missing part1.h file added

---
+++ HW3/inc/Part1.h
@@ -0,0 +1,21 @@
+/*
+ * Part1.h
+ *
+ *  Created on: Mar 9, 2020
+ *      Author: onur
+ */
+
+#ifndef INC_PART1_H_
+#define INC_PART1_H_
+
+#define SUM_OPERATION 0
+#define MULT_OPERATION 1
+
+#define EVEN_NUMBERS 0
+#define ODD_NUMBERS 1
+
+void sum_mult_operation();
+int sum(int n, int flag);
+int mult(int n, int flag);
+
+#endif /* INC_PART1_H_ */
