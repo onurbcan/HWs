fc0c972d737c20ae8ee4749b2ded8b8c342dc129
Onur <onur.can@ozu.edu.tr>
2020-03-23 12:07:04 +0100

    initial push for HWs_HW4_Part2_Implementation

---
+++ HW4/inc/Part2.h
@@ -0,0 +1,25 @@
+/*
+ * Part1.h
+ *
+ *  Created on: Mar 19, 2020
+ *      Author: onur
+ */
+
+#ifndef INC_PART1_H_
+#define INC_PART1_H_
+
+#define ZERO 48
+#define ONE 49
+#define TWO 50
+#define THREE 51
+#define FOUR 52
+#define FIVE 53
+#define SIX 54
+
+FILE *fptr;
+
+void open_file(char *file_path);
+void close_file();
+void deep_decrypt_and_print (char* file_path);
+
+#endif /* INC_PART1_H_ */
---
+++ HW4/src/Part2.c
@@ -0,0 +1,47 @@
+/*
+ * Part2.c
+ * Description: They say that there could be another message if the numbers in
+ * the file is replaced with the numbers gained from the formula:
+ * 2
+ * Number i = (∑ Number i+j ) % 7
+ * j=0
+ * 0 1 2 0 5 1 2 0 5 ...
+ * 3 3 0 6 1 ...
+ * You are asked again to write a function that prints that deep secret messages
+ * in such files. By the formula, if there are no longer available three number
+ * for any position such as end of lines, you can assume the value of missing
+ * numbers are 0. Use encrypted_p2.img file to test your decryption function.
+ * Don’t change the original file, just apply the formula when you print the
+ * actual character. The function prototype has given below:
+ *
+ * void deep_decrypt_and_print (char* file_path)
+ *
+ *  Created on: Mar 23, 2020
+ *      Author: onur
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Part2.h"
+
+void open_file(char *file_path) {
+	if ((fptr = fopen(file_path, "r")) == NULL) {
+		printf("Error! File not found.");
+		exit(1);
+	}
+	return;
+}
+
+void close_file() {
+	if (fclose(fptr) != 0) {
+		printf("Error! File not closed.");
+		exit(1);
+	}
+	return;
+}
+
+void deep_decrypt_and_print(char *file_path) {
+	open_file(file_path);
+	close_file();
+	return;
+}
