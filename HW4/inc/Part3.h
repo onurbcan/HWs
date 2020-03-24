75f2cf9ce0a0ab3cd6861b3f54ffb9e2ae150609
Onur <onur.can@ozu.edu.tr>
2020-03-24 16:41:53 +0100

    part3 in progress 2

--- HW4/inc/Part3.h
+++ HW4/inc/Part3.h
@@ -8,6 +8,11 @@
 #ifndef INC_PART3_H_
 #define INC_PART3_H_
 
+#define OUR_X_COORDINATE 6
+#define OUR_Y_COORDINATE 6
+
+int temp_X, temp_Y;
+
 void track_machine();
 void refresh_position (int *X, int *Y, double *D, double *R);
 
--- HW4/src/Part3.c
+++ HW4/src/Part3.c
@@ -27,6 +27,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
 #include "Part3.h"
 
 void track_machine(){
@@ -35,19 +37,57 @@ void track_machine(){
 
 	}while();
 	*/
-	int i, j;
+	int X = 0,
+		Y = 0,
+		in;
 
-	for(i = 1 ; i <= 11 ; ++i){
-		for(j = 1 ; j <= 11 ; ++j){
-			printf(".\t");
-		}
-		printf("\n");
-	}
+	double R, //distance between current position and the last position of the enemy camp
+		D; //distance of enemy to our camp
 
+	do{
+	refresh_position(&X, &Y, &D, &R);
+	printf("Xtra: %d\n", X);
+	printf("Ytra: %d\n", Y);
+	printf("D: %f\n", D);
+	printf("R: %f\n", R);
+	printf("in value: ");
+	scanf("%d", &in);
+	}while(in != 0);
 	return;
 }
 
 void refresh_position (int *X, int *Y, double *D, double *R){
+	int i, j;
+
+	srand(time(NULL));
+
+	printf("Xref: %d\n", *X);
+	printf("Yref: %d\n", *Y);
+
+	temp_X = *X;
+	temp_Y = *Y;
+
+	*X = (rand() % 11) + 1;
+	*Y = (rand() % 11) + 1;
+
+
+
+
+	for(i = 1 ; i <= 11 ; ++i){ //y - coordinate
+		for(j = 1 ; j <= 11 ; ++j){ //x - coordinate
+			if(i == OUR_X_COORDINATE && j == OUR_Y_COORDINATE){
+				printf("O\t");
+			}else if(i == *X && j == *Y){
+				printf("E\t");
+			}else{
+				printf(".\t");
+			}
+		}
+		printf("\n");
+	}
+
+	*D = sqrt(pow((*X - temp_X),2) + pow((*Y - temp_Y),2));
+	*R = sqrt(pow((*X - OUR_X_COORDINATE),2) + pow((*Y - OUR_Y_COORDINATE),2));
 
 	return;
 }
