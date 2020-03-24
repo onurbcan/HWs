6fffd96c386bbd7533de4b83515b7ee4ca27b3cb
Onur <onur.can@ozu.edu.tr>
2020-03-24 19:45:40 +0100

    part3 completed 2

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -14,7 +14,7 @@
 #include "Part3.h"
 
 int main(int argc, char **argv) {
-	//decrypt_and_print(argv[1]);
+	decrypt_and_print(argv[1]);
 	track_machine();
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
