32ba8a01d0122bb1ca94e2aa354ce8f4a93279b2
Onur <onur.can@ozu.edu.tr>
2020-03-29 19:33:37 +0200

    part5 in progress

--- HW4/src/Part5.c
+++ HW4/src/Part5.c
@@ -1,5 +1,13 @@
 /*
  * Part5.c
+ * Description: Write a menu function to operate all the three parts. The menu
+ * should work forever until the operator selects the exit option. Your menu
+ * should seem as the figure below:
+ *
+ * The menu have to be in a function and your main function must contains only
+ * the menu function (and may be SRAND() function). The function prototype is:
+ *
+ * void menu ()
  *
  *  Created on: Mar 29, 2020
  *      Author: onur
@@ -11,6 +19,27 @@
 #include "Part5.h"
 
 void menu() {
+	int choice;
 
+	printf("1-) Decrypt and print encrypted_p1.img\n");
+	printf("2-) Decrypt and print encrypted_p2.img\n");
+	printf("3-) Switch on the tracking machine\n");
+	printf("4-) Encrypt the message\n");
+	printf("5-) Switch off\n");
+	printf("\n");
+	printf("Please make your choice:\n");
+	scanf("%d",&choice);
+	return;
+}
+
+void menu_cases(int menu_index){
+	switch (menu_index) {
+	case (0):
+		decrypt_and_print("files/encrypted_p1.img");
+	case (1):
+		deep_decrypt_and_print("files/encrypted_p2.img");
+	case (2):
+
+		"files/decrypted_p4.img"
 	return;
 }
