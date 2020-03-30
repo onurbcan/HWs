61b8b4fbd0a2b01fc9cb77984786c74581112447
Onur <onur.can@ozu.edu.tr>
2020-03-30 21:48:50 +0200

    part5 irrelevant hw5 files eliminated

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -17,10 +17,10 @@
 #include "Part5.h"
 
 int main(int argc, char **argv) {
-	//decrypt_and_print(argv[1]);
-	//deep_decrypt_and_print(argv[2]);
-	//track_machine();
-	//encrypt_messages (argv[3]);
+	decrypt_and_print(argv[1]);
+	deep_decrypt_and_print(argv[2]);
+	track_machine();
+	encrypt_messages (argv[3]);
 	menu();
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
