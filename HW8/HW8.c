474a6db20c232387ad71356067d34b57faa41b27
Onur <onur.can@ozu.edu.tr>
2020-04-26 12:17:43 +0200

    first stash for hw8

---
+++ HW8/HW8.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : HW8.c
+ Author      : Onur
+ Version     :
+ Copyright   :
+ Description : HW8
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	return EXIT_SUCCESS;
+}
