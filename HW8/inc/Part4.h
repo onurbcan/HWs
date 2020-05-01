b5b91a2897aba674db573493ea18ff0eb2a95397
Onur <onur.can@ozu.edu.tr>
2020-05-01 21:44:19 +0200

    menu screen and cases developed but sections for part3 in progress

---
+++ HW8/inc/Part4.h
@@ -0,0 +1,17 @@
+/*
+ * Part4.h
+ *
+ *  Created on: May 1, 2020
+ *      Author: onur
+ */
+
+#ifndef INC_PART4_H_
+#define INC_PART4_H_
+
+/*! 4.1.0) Menu Screen */
+void menu();
+
+/*! 4.2.0) Menu Cases */
+void menu_cases(int menu_choice, int *if_break);
+
+#endif /* INC_PART4_H_ */
