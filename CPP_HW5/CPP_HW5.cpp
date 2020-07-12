6df9d249d4c3794f2a566005f8cf27326e209013
Onur <onur.can@ozu.edu.tr>
2020-07-12 11:10:56 +0200

    first stash for the initial file

---
+++ CPP_HW5/CPP_HW5.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : CPP_HW5.cpp
+// Author      : Onur
+// Version     :
+// Copyright   : 
+// Description : N-Puzzle game (Inheritance)
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	return 0;
+}
